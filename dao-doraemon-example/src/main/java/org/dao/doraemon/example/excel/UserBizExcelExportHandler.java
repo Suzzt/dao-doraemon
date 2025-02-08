67dff3decf073a68df3a7a00d5176186191586ae
sucf <1058772978@qq.com>
2025-02-08 16:17:23 +0800

    feat: export excel data.

---
+++ dao-doraemon-example/src/main/java/org/dao/doraemon/example/excel/UserBizExcelExportHandler.java
@@ -0,0 +1,11 @@
+package org.dao.doraemon.example.excel;
+
+import org.dao.doraemon.excel.annotation.ExcelExport;
+
+/**
+ * @author sucf
+ * @since 1.0
+ */
+@ExcelExport(code = "user")
+public class UserBizExcelExportHandler {
+}
