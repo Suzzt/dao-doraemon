b2125a49a91767f3ae4295788799ca24c940fac4
sucf <1058772978@qq.com>
2025-04-01 17:25:36 +0800

    [bug]: Jackson在测试工程中定义加载冲突.

--- dao-doraemon-example/src/main/java/org/dao/doraemon/example/database/config/BeanConfig.java
+++ dao-doraemon-example/src/main/java/org/dao/doraemon/example/database/config/BeanConfig.java
@@ -33,22 +33,22 @@ public class BeanConfig {
         };
     }
 
-    @Bean
-    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
-        return jacksonObjectMapperBuilder ->
-            jacksonObjectMapperBuilder.simpleDateFormat("yyyy-MM-dd HH:mm:ss")
-                // long类型转string， 前端处理Long类型，数值过大会丢失精度
-                .serializerByType(Long.class, ToStringSerializer.instance)
-                .serializerByType(Long.TYPE, ToStringSerializer.instance)
-                .serializationInclusion(JsonInclude.Include.NON_NULL)
-                //指定反序列化类型，也可以使用@JsonFormat(pattern = "yyyy-MM-dd")替代。主要是mvc接收日期时使用
-                .deserializerByType(LocalDate.class,
-                    new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-                .deserializerByType(LocalDateTime.class,
-                    new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                // 日期序列化，主要返回数据时使用
-                .serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
-                .serializerByType(LocalDateTime.class,
-                    new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-    }
+//    @Bean
+//    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+//        return jacksonObjectMapperBuilder ->
+//            jacksonObjectMapperBuilder.simpleDateFormat("yyyy-MM-dd HH:mm:ss")
+//                // long类型转string， 前端处理Long类型，数值过大会丢失精度
+//                .serializerByType(Long.class, ToStringSerializer.instance)
+//                .serializerByType(Long.TYPE, ToStringSerializer.instance)
+//                .serializationInclusion(JsonInclude.Include.NON_NULL)
+//                //指定反序列化类型，也可以使用@JsonFormat(pattern = "yyyy-MM-dd")替代。主要是mvc接收日期时使用
+//                .deserializerByType(LocalDate.class,
+//                    new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+//                .deserializerByType(LocalDateTime.class,
+//                    new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
+//                // 日期序列化，主要返回数据时使用
+//                .serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+//                .serializerByType(LocalDateTime.class,
+//                    new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+//    }
 }
